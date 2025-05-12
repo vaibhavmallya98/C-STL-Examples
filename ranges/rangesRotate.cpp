7343605230a246d067633312e6aa67113796dbe3
vaibhavmallya98 <45683079+vaibhavmallya98@users.noreply.github.com>
2025-05-12 10:57:38 +0530

    Update rangesRotate.cpp

--- ranges/rangesRotate.cpp
+++ ranges/rangesRotate.cpp
@@ -7,10 +7,13 @@
 int main()
 {
     std::string s(16, ' ');
- 
+
+    
     for (int k {}; k != 5; ++k)
     {
+        //std::iota over here will create the string ABCDEFGHIJKLMNOP as the length of the string is 16
         std::iota(s.begin(), s.end(), 'A');
+        //this is for rotate left 
         std::ranges::rotate(s, s.begin() + k);
         std::cout << "Rotate left (" << k << "): " << s << '\n';
     }
@@ -19,6 +22,7 @@ int main()
     for (int k {}; k != 5; ++k)
     {
         std::iota(s.begin(), s.end(), 'A');
+        //this is for rotate right 
         std::ranges::rotate(s, s.end() - k);
         std::cout << "Rotate right (" << k << "): " << s << '\n';
     }
@@ -29,9 +33,12 @@ int main()
  
     for (auto i = s.begin(); i != s.end(); ++i)
     {
+        //ranges::distance will print the number of elements within a range 
         std::cout << "i = " << std::ranges::distance(s.begin(), i) << ": ";
         std::ranges::rotate(std::ranges::upper_bound(s.begin(), i, *i), i, i + 1);
         std::cout << s << '\n';
     }
+
+    ///ranges::is_sorted checks if the entire range is sorted 
     std::cout << (std::ranges::is_sorted(s) ? "Sorted!" : "Not sorted.") << '\n';
 }
